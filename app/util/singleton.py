2a736a43e9117febdfec90b08fa79b6bc1f42301
agingrasc <alexandre.gingras-courchesne.1@ulaval.ca>
2017-04-02 16:33:15 -0400

    unit test singleton

--- app/util/singleton.py
+++ app/util/singleton.py
@@ -5,3 +5,12 @@ class Singleton(type):
         if cls not in cls._instances:
             cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
         return cls._instances[cls]
+
+
+class MockForSingleton(metaclass=Singleton):
+
+    def __init__(self):
+        self.first_value = 0
+        self.second_value = 0
+
+
