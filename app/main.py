b91a3867706f83837dcd37fcd8e5ebd1077eb39e
jsletellier <js.letellier@gmail.com>
2017-04-05 11:55:07 -0400

    Fix, put the normal port

--- app/main.py
+++ app/main.py
@@ -73,7 +73,7 @@ def main():
     else:
         print("Bad arguments : manual or automatic")
 
-    app.run(host='0.0.0.0', port=6000)
+    app.run(host='0.0.0.0', port=5000)
 
 
 if __name__ == '__main__':
