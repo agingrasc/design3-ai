989ff98b6f1b6bb0692bfb6e3e42c60ddea80e98
agingrasc <alexandre.gingras-courchesne.1@ulaval.ca>
2017-04-09 22:47:10 -0400

    ajout tests pour position et fonctions utilitaires (repr, str, eq)

--- app/domain/gameboard/position.py
+++ app/domain/gameboard/position.py
@@ -18,7 +18,7 @@ class Position:
         return np.arctan2(float(-self.pos_y), float(self.pos_x))
 
     def multiply(self, operand):
-        return Position(self.pos_x * operand, self.pos_y * operand, self.theta)
+        return Position(int(self.pos_x * operand), int(self.pos_y * operand), self.theta)
 
     def renormalize(self, norm):
         scale_factor = norm / self.get_norm()
@@ -29,3 +29,12 @@ class Position:
 
     def __sub__(self, other):
         return Position(self.pos_x - other.pos_x, self.pos_y - other.pos_y, self.theta)
+
+    def __eq__(self, other):
+        return self.pos_x == other.pos_x and self.pos_y == other.pos_y and self.theta == other.theta
+
+    def __str__(self):
+        return "Position: ({}, {}, {})".format(self.pos_x, self.pos_y, self.theta)
+
+    def __repr__(self):
+        return "Position({}, {}, {})".format(self.pos_x, self.pos_y, self.theta)
