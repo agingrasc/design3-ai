7fabd0928173bac6a09d41978952332fa2934668
nathaniel <nathaniel.simard.1@ulaval.ca>
2017-04-11 11:30:52 -0400

    refine tests

--- app/service/safezonefinder.py
+++ app/service/safezonefinder.py
@@ -8,16 +8,13 @@ STOP_POSITION_LIST = [Position(1250, 580, 0), Position(1250, 880, 0), Position(1
 
 
 class SafeZoneFinder():
-    def __init__(
-        self, pathfinding_application_service, global_information: GlobalInformation, positions=STOP_POSITION_LIST
-    ):
+    def __init__(self, pathfinding_application_service, global_information: GlobalInformation):
         self.pathfinder_service = pathfinding_application_service
         self.global_information = global_information
         self.list_path = []
-        self.positions = positions
 
-    def find_safe_zone(self):
-        for position in self.positions:
+    def find_safe_zone(self, positions=STOP_POSITION_LIST):
+        for position in positions:
             try:
                 path = self.pathfinder_service.find(self.global_information, position)
                 if not len(path) == 0 and path[len(path) - 1] == position:
@@ -26,6 +23,7 @@ class SafeZoneFinder():
                 pass
 
         if len(self.list_path) == 0:
+            print("Find safer zone")
             return self.find_safe_zone(POSITIONS)
 
         path_to_return = self.list_path[0]
--- app/service/test_safezonefinder.py
+++ app/service/test_safezonefinder.py
@@ -2,21 +2,14 @@ from unittest import TestCase
 from unittest.mock import Mock
 from service.safezonefinder import SafeZoneFinder
 from service.safezonefinder import STOP_POSITION_LIST
+from domain.gameboard.position import Position
 
-VALID_PATH_1 = [
-    4,
-    6,
-    7,
-    STOP_POSITION_LIST[0],
-]
-VALID_PATH_2 = [
-    2,
-    3,
-    4,
-    6,
-    7,
-    STOP_POSITION_LIST[1],
-]
+STOP_POSITION_LIST = [Position(1250, 580, 0), Position(1250, 880, 0)]
+
+VALID_PATH_1 = [Mock(), STOP_POSITION_LIST[0]]
+NOT_VALID_PATH_1 = [Mock(), Mock()]
+VALID_PATH_2 = [Mock(), Mock(), STOP_POSITION_LIST[1]]
+NOT_VALID_PATH_2 = [Mock(), Mock(), Mock()]
 
 
 class SafeZoneFinderTest(TestCase):
@@ -26,11 +19,16 @@ class SafeZoneFinderTest(TestCase):
         self.safe_zone_finder_test = SafeZoneFinder(self.pathfinding_application_service, self.global_information)
 
     def test_find_shorter_safe_zone_2_valid_paths(self):
-        self.pathfinding_application_service.find.side_effect = [VALID_PATH_1, VALID_PATH_2, [], []]
-        path = self.safe_zone_finder_test.find_safe_zone()
+        self.pathfinding_application_service.find.side_effect = [VALID_PATH_1, VALID_PATH_2]
+        path = self.safe_zone_finder_test.find_safe_zone(STOP_POSITION_LIST)
         self.assertEqual(VALID_PATH_1, path)
 
-    def test_find_shorter_safe_zone_1_valid_path(self):
-        self.pathfinding_application_service.find.side_effect = [[], VALID_PATH_2, [], []]
-        path = self.safe_zone_finder_test.find_safe_zone()
+    def test_find_shorter_safe_zone_1_valid_path1(self):
+        self.pathfinding_application_service.find.side_effect = [VALID_PATH_1, NOT_VALID_PATH_2]
+        path = self.safe_zone_finder_test.find_safe_zone(STOP_POSITION_LIST)
+        self.assertEqual(VALID_PATH_1, path)
+
+    def test_find_shorter_safe_zone_1_valid_path2(self):
+        self.pathfinding_application_service.find.side_effect = [NOT_VALID_PATH_1, VALID_PATH_2]
+        path = self.safe_zone_finder_test.find_safe_zone(STOP_POSITION_LIST)
         self.assertEqual(VALID_PATH_2, path)
