4f25c5b421d1ed02e98a55f30b9c3fc9bdc576ac
agingrasc <alexandre.gingras-courchesne.1@ulaval.ca>
2017-04-12 12:42:53 -0400

    ajout methode pour trouver l'angle le plus proche et tests pour valider

--- app/domain/command/drawer.py
+++ app/domain/command/drawer.py
@@ -33,19 +33,35 @@ class Drawer:
         time.sleep(WAIT_TIME)
         self.robot_controller.lower_pencil()
         for destination in segments[1:]:
-            angle = self.compute_draw_angle(destination) - draw_angle
+            angle = self.compute_draw_angle(destination, draw_angle)
             self.vision_regulation.oriente_robot(angle)
             destination.theta = angle
             self.vision_regulation.go_to_position(destination)
 
         self.stop()
 
-    def compute_draw_angle(self, destination: Position) -> float:
+    def compute_draw_angle(self, destination: Position, draw_angle) -> float:
         robot_position = self.global_information.get_robot_position()
+        robot_angle = robot_position.theta
         direction_vector = destination - robot_position
-        angle = direction_vector.get_angle()
-        return wrap_theta(angle)
+        angle = direction_vector.get_angle() - draw_angle
+        nearest_angle = self._get_nearest_angle(angle, robot_angle)
+        return wrap_theta(nearest_angle)
 
     def stop(self):
         self.robot_controller.raise_pencil()
         self.robot_controller.set_robot_speed(RobotSpeed.NORMAL_SPEED)
+
+
+def _get_nearest_angle(angle, robot_angle):
+    valid_angles = [wrap_theta((angle + np.deg2rad(90 * i))) for i in range(4)]
+    shortest_delta = np.pi
+    shortest_idx = 0
+
+    for idx, angle in enumerate(valid_angles):
+        delta_angle = abs(wrap_theta(angle - robot_angle))
+        if delta_angle < shortest_delta:
+            shortest_delta = delta_angle
+            shortest_idx = idx
+    return valid_angles[shortest_idx]
+
